17a6d6d8ceba26dcc02835516a942a34e1b8934e
Durga Prasad Bejawada <117895509+Durga0808@users.noreply.github.com>
2024-06-11 10:54:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //your JS code here. If required.
 // script.js
 
-document.getElementById('change').addEventListener('click', function() {
+document.getElementById('change_button').addEventListener('click', function() {
     // Get the block ID and color from the input fields
     const blockId = document.getElementById('block_id').value;
     const color = document.getElementById('colour_id').value;
